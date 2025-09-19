GNOME/gnome-mahjongg

commit e1dd6f4e58b379e6cfc5e38a7474166ca3245ab3

Use window.close() instead of window.destroy()

src/gnome-mahjongg.vala
@@ -375,7 +375,8 @@ Copyright © 1998–2008 Free Software Foundation, Inc.""",
     }
 
     private void quit_cb () {
-        window.destroy ();
+        if (window != null)
+            window.close ();
     }
 
     private void redo_cb () {

commit 466ffc9116237a6acf8c554b85e1e86e359ee7b1

Ensure the application quits when requested
Trying to close a window will close an open dialog first. This
prevented the Quit button in the Scores dialog from quitting the
application.

Regression in e1dd6f4e58b379e6cfc5e38a7474166ca3245ab3

Fixes #68

src/gnome-mahjongg.vala
@@ -28,7 +28,7 @@ public class Mahjongg : Adw.Application {
         { "theme", null, "s", "''", theme_cb },
         { "rules", rules_cb },
         { "about", about_cb },
-        { "quit", quit_cb }
+        { "quit", quit }
     };
 
     public Mahjongg () {
@@ -190,7 +190,7 @@ public class Mahjongg : Adw.Application {
                 break;
             case "quit":
                 game_save.delete ();
-                active_window.close ();
+                quit ();
                 break;
             default:
                 break;
@@ -353,11 +353,6 @@ Copyright © 1998–2008 Free Software Foundation, Inc.""",
     private void restart_game_cb () {
         game.restart ();
         game_save.delete ();
-    }
-
-    private void quit_cb () {
-        if (active_window != null)
-            active_window.close ();
     }
 
     private void redo_cb () {